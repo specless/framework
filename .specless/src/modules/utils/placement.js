59e9b375028a68c2d27855d4884a1037e634f169
Shashank <shashank@gospecless.com>
2020-10-02 11:08:52 -0500

    fix parseVast pathname

--- .specless/src/modules/utils/placement.js
+++ .specless/src/modules/utils/placement.js
@@ -1,4 +1,4 @@
 export { panelUrl } from './../../utils/panelUrl.js';
-export { parseVAST } from './../../utils/parseVAST.js';
+export { parseVAST } from './../../utils/parseVast.js';
 export { createUseStyles } from 'react-jss';
 export { getLayout } from './../../utils/getLayout.js';
